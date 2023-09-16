32a2faac716218349f0ce82372304dbea693720d
yuriy-npm <gnatko.yr@gmail.com>
2023-09-16 16:48:59 +0300

    changed server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const path = require('path')
-const cors=require("cors");
+
 
 
 const PORT = process.env.PORT || 5000
@@ -12,12 +12,7 @@ app.use(express.static(path.resolve(__dirname, 'build')))
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'build', 'index.html'))
 })
-const corsOptions ={
-    origin:'*', 
-    credentials:true,            //access-control-allow-credentials:true
-    optionSuccessStatus:200,
- }
- 
- app.use(cors(corsOptions))
+
+
 
 app.listen(PORT)
