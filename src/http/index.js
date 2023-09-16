c37ff0ac952b4b54939b4e1b1c9bba335a3fc0d6
yuriy-npm <141719920+yuriy-npm@users.noreply.github.com>
2023-09-16 15:17:06 +0300

    Update index.js

--- src/http/index.js
+++ src/http/index.js
@@ -1,8 +1,11 @@
 import axios from 'axios'
 
 // console.log(process.env.REACT_APP_API_URL);
-export const BASE_URL = "http://localhost:5000/"
-const REACT_APP_API_URL = "http://localhost:5000/"
+// export const BASE_URL = "http://localhost:5000/"
+// const REACT_APP_API_URL = "http://localhost:5000/"
+
+export const BASE_URL = "https://barber-book-app-31a74090aa3c.herokuapp.com/"
+const REACT_APP_API_URL = "https://barber-book-app-31a74090aa3c.herokuapp.com/"
 
 const $host = axios.create({
     baseURL: REACT_APP_API_URL
